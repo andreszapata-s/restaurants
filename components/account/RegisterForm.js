acd65abbf68b97fab753554b970737cde65e3583
andreszapata-s <andreszapata9804@gmail.com>
2021-03-04 21:24:44 -0500

    Save data form in a state

--- components/account/RegisterForm.js
+++ components/account/RegisterForm.js
@@ -5,18 +5,27 @@ import { Button, Icon, Input } from 'react-native-elements'
 export default function RegisterForm() {
     
     const [showPassword, setShowPassword] = useState(false)
+    const [formData, setFormData] = useState(defaultFormValues())
+
+    const onChange = (e, type) => {
+        setFormData({...formData, [type]: e.nativeEvent.text}) //El type entre corchetes significa que es dinamico
+    }
+
 
     return (
         <View style={styles.form}>
             <Input
                 containerStyle={styles.input}
                 placeholder="Ingresa tu email..."
+                onChange={(e) =>onChange(e,"email")}
+                keyboardType="email-address"
             />
             <Input
                 containerStyle={styles.input}
                 placeholder="Ingresa tu contraseña..."
                 password={true}
                 secureTextEntry={!showPassword}
+                onChange={(e) =>onChange(e,"password")}
                 rightIcon={
                     <Icon
                         type="material-community"
@@ -31,6 +40,7 @@ export default function RegisterForm() {
                 placeholder="Confirma tu contraseña..."
                 password={true}
                 secureTextEntry={!showPassword}
+                onChange={(e) =>onChange(e,"confirm")}
                 rightIcon={
                     <Icon
                         type="material-community"
@@ -44,9 +54,14 @@ export default function RegisterForm() {
                 title="Registrar nuevo usuario"
                 containerStyle={styles.btnContainer}
                 buttonStyle={styles.btn}
+                onPress={() => console.log(formData)}
             />
         </View>
     )
+}
+
+const defaultFormValues = () =>{
+    return {email : "", password : "", confirm : ""}
 }
 
 const styles = StyleSheet.create({
