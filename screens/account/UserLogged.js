663453bdd9128acfb3263b3e21027bc3f60125b5
andreszapata-s <andreszapata9804@gmail.com>
2021-03-08 21:32:24 -0500

    Revert "Login"
    This reverts commit c276b6d4fa3abab61afb795adccac6059542dcf6.

--- screens/account/UserLogged.js
+++ screens/account/UserLogged.js
@@ -1,67 +1,26 @@
-import React, {useState, useRef, useEffect} from 'react'
+import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Button } from 'react-native-elements'
-import  Toast  from 'react-native-easy-toast'
 
-
-import { useNavigation } from "@react-navigation/native"
-import { closeSession, getCurrentUser } from '../../utils/actions'
-import Loading from '../../components/Loading'
-import InfoUser from '../../components/account/InfoUser'
-
+import { useNavigation } from "@react-navigation/native";
+import { closeSession } from '../../utils/actions';
 
 export default function UserLogged() {
-
-    const toastRef = useRef()
     const navigation = useNavigation()
     
-    const [loading, setLoading] = useState(false)
-    const [loadingText, setLoadingText] = useState("")
-    const [user, setUser] = useState(null)
-
-    useEffect(() => {
-        setUser(getCurrentUser())
-    }, [])
-
     return (
 
-        <View style={styles.container}>
-            {
-                user &&<InfoUser user={user}/>
-            }
-            <Text>Account options</Text>
+        <View>
+            <Text>User Logged</Text>
             <Button
                 title="Cerrar sesion"
-                buttonStyle={styles.btnCloseSession}
-                titleStyle={styles.btnCloseSessionTittle}
                 onPress={()=>{
                     closeSession()
                     navigation.navigate("restaurants")
                 }}
             />
-            <Toast ref={toastRef} position="center" opacity={0.9}/>
-            <Loading isVisible={loading} text={loadingText}/>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    container:{
-        minHeight:"100%",
-        backgroundColor:"#f9f9f9"
-    },
-    btnCloseSession:{
-        marginTop: 30,
-        borderRadius:5,
-        backgroundColor:"#FFFFFF",
-        borderTopWidth:1,
-        borderTopColor:"#442484",
-        borderBottomWidth:1,
-        borderBottomColor:"#442484",
-        paddingVertical: 10
-
-    },
-    btnCloseSessionTittle:{
-        color: "#442484"
-    }
-})
+const styles = StyleSheet.create({})
