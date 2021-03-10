b9fb042d61a7b083a39f09130bd5acfe297c95a7
andreszapata-s <andreszapata9804@gmail.com>
2021-03-09 20:28:36 -0500

    Add user options

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { LogBox } from 'react-native';
 
 import Navigation from './navigations/Navigation'
+
+LogBox.ignoreAllLogs()
 
 export default function App() {
   return (
--- screens/account/UserLogged.js
+++ screens/account/UserLogged.js
@@ -7,6 +7,7 @@ import Toast from 'react-native-easy-toast'
 import { closeSession, getCurrentUser } from '../../utils/actions'
 import Loading from '../../components/Loading'
 import InfoUser from '../../components/account/InfoUser'
+import AccountOptions from '../../components/account/AccountOptions'
 
 export default function UserLogged() {
     const toastRef = useRef()
@@ -27,8 +28,13 @@ export default function UserLogged() {
             {
                 user && (
                 <View>
-                    <InfoUser user={user} setLoading={setLoading} setLoadingText={setLoadingText}/>
-                    <Text>Account options</Text>
+                    <InfoUser 
+                     user={user}
+                     setLoading={setLoading} setLoadingText={setLoadingText}/>
+                    <AccountOptions
+                      user={user}
+                      toastRef={toastRef}
+                    />
                 </View>
                 )
             }
