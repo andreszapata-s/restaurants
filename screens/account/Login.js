a3f7fc4de9803cfd0a5821902904d10e967a88d2
andreszapata-s <andreszapata9804@gmail.com>
2021-05-02 15:08:53 -0500

    Add recover password screen

--- screens/account/Login.js
+++ screens/account/Login.js
@@ -18,12 +18,30 @@ export default function Login() {
             <View style={styles.container}>
                 <LoginForm/>
                 <CreateAccount/>
+                <RecoverPassword/>
             </View>
             <Divider
                 style={styles.divider}
             />
         </KeyboardAwareScrollView>
     )
+}
+
+function RecoverPassword (){
+    const navigation = useNavigation()
+
+    return (
+        <Text 
+            style = {styles.register}
+            onPress={() => navigation.navigate("recover-password")}
+        >
+            ¿Olvidaste tu contraseña?{" "}
+            <Text style={styles.btnRegister}>
+                Recuperala
+            </Text>
+        </Text>
+    )
+
 }
 
 function CreateAccount(props){
